84721d045f46a72400a14f4fec8ac06054f45545
laogai <1432681790@qq.com>
2017-10-28 17:53:40 +0800

    修改我的订单

--- app/Http/Controllers/Home/UserController.php
+++ app/Http/Controllers/Home/UserController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Home;
 
 use App\Http\Model\DistributionRecord2;
+use App\Http\Model\Goods2;
+use App\Http\Model\Order2;
+use App\Http\Model\Orderinfo2;
 use Illuminate\Http\Request;
 
 use App\Http\Model\Config2;
@@ -1226,9 +1229,9 @@ try{
     static function userorderinfo($i,$uid){
        
        
-        $list=Order::where('user_id',$uid)->where('status',$i)->orderBy('id','desc')->get();
+        $orders1=Order::where('user_id',$uid)->where('status',$i)->orderBy('id','desc')->get();
 
-        foreach ($list as $k => $v) {
+        foreach ($orders1 as $k => $v) {
             $info=Orderinfo::select(['order_id','price','num','name','goods_id'])->where('order_id',$v['id'])->get();
                 foreach ($info as $key => $value) {
                     $goods=Goods::select(['id','pic','name'])->where('id',$value['goods_id'])->first();
@@ -1237,9 +1240,29 @@ try{
 
                 }
 
-                $list[$k]->info = $info;
+                $orders1[$k]->info = $info;
 
         
+        }
+        $orders2=Order2::where('user_id',$uid)->where('status',$i)->orderBy('id','desc')->get();
+
+        foreach ($orders2 as $k => $v) {
+            $info=Orderinfo2::select(['order_id','price','num','name','goods_id'])->where('order_id',$v['id'])->get();
+                foreach ($info as $key => $value) {
+                    $goods=Goods2::select(['id','pic','name'])->where('id',$value['goods_id'])->first();
+                    $info[$key]['name']=$goods['name'];
+                    $info[$key]['pic']=$goods['pic'];
+
+                }
+
+            $orders2[$k]->info = $info;
+
+
+        }
+        $list = $orders1->merge($orders2);
+
+        if($i == 1){
+            return $orders1;
         }
         return $list;
     }
